e66d24d829d40aa7bbe0d8e4d7654fbc594a793c
Deniz Yılmaz <31761784+xor-128@users.noreply.github.com>
2022-04-26 04:50:11 +0300

    Add files via upload

---
+++ CustomRules.js
@@ -0,0 +1,99 @@
+﻿import System;
+import System.Windows.Forms;
+import Fiddler;
+
+class Handlers
+{
+    public static RulesOption("Hide 304s")
+    BindPref("fiddlerscript.rules.Hide304s")
+    var m_Hide304s: boolean = false;
+        
+    public static RulesOption("Request &Japanese Content")
+    var m_Japanese: boolean = false;
+
+    public static RulesOption("&Automatically Authenticate")
+    BindPref("fiddlerscript.rules.AutoAuth")
+    var m_AutoAuth: boolean = false;
+
+    RulesString("&User-Agents", true) 
+    BindPref("fiddlerscript.ephemeral.UserAgentString")
+    RulesStringValue(0,"Netscape &3", "Mozilla/3.0 (Win95; I)")
+    RulesStringValue(1,"WinPhone8.1", "Mozilla/5.0 (Mobile; Windows Phone 8.1; Android 4.0; ARM; Trident/7.0; Touch; rv:11.0; IEMobile/11.0; NOKIA; Lumia 520) like iPhone OS 7_0_3 Mac OS X AppleWebKit/537 (KHTML, like Gecko) Mobile Safari/537")
+    RulesStringValue(2,"&Safari5 (Win7)", "Mozilla/5.0 (Windows; U; Windows NT 6.1; en-US) AppleWebKit/533.21.1 (KHTML, like Gecko) Version/5.0.5 Safari/533.21.1")
+    RulesStringValue(3,"Safari9 (Mac)", "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11) AppleWebKit/601.1.56 (KHTML, like Gecko) Version/9.0 Safari/601.1.56")
+    RulesStringValue(4,"iPad", "Mozilla/5.0 (iPad; CPU OS 8_3 like Mac OS X) AppleWebKit/600.1.4 (KHTML, like Gecko) Version/8.0 Mobile/12F5027d Safari/600.1.4")
+    RulesStringValue(5,"iPhone6", "Mozilla/5.0 (iPhone; CPU iPhone OS 8_3 like Mac OS X) AppleWebKit/600.1.4 (KHTML, like Gecko) Version/8.0 Mobile/12F70 Safari/600.1.4")
+    RulesStringValue(6,"IE &6 (XPSP2)", "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; SV1)")
+    RulesStringValue(7,"IE &7 (Vista)", "Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 6.0; SLCC1)")
+    RulesStringValue(8,"IE 8 (Win2k3 x64)", "Mozilla/4.0 (compatible; MSIE 8.0; Windows NT 5.2; WOW64; Trident/4.0)")
+    RulesStringValue(9,"IE &8 (Win7)", "Mozilla/4.0 (compatible; MSIE 8.0; Windows NT 6.1; Trident/4.0)")
+    RulesStringValue(10,"IE 9 (Win7)", "Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.1; Trident/5.0)")
+    RulesStringValue(11,"IE 10 (Win8)", "Mozilla/5.0 (compatible; MSIE 10.0; Windows NT 6.2; WOW64; Trident/6.0)")
+    RulesStringValue(12,"IE 11 (Surface2)", "Mozilla/5.0 (Windows NT 6.3; ARM; Trident/7.0; Touch; rv:11.0) like Gecko")
+    RulesStringValue(13,"IE 11 (Win8.1)", "Mozilla/5.0 (Windows NT 6.3; WOW64; Trident/7.0; rv:11.0) like Gecko")
+    RulesStringValue(14,"Edge (Win10)", "Mozilla/5.0 (Windows NT 10.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/46.0.2486.0 Safari/537.36 Edge/13.11082")
+    RulesStringValue(15,"&Opera", "Opera/9.80 (Windows NT 6.2; WOW64) Presto/2.12.388 Version/12.17")
+    RulesStringValue(16,"&Firefox 3.6", "Mozilla/5.0 (Windows; U; Windows NT 6.1; en-US; rv:1.9.2.7) Gecko/20100625 Firefox/3.6.7")
+    RulesStringValue(17,"&Firefox 43", "Mozilla/5.0 (Windows NT 6.3; WOW64; rv:43.0) Gecko/20100101 Firefox/43.0")
+    RulesStringValue(18,"&Firefox Phone", "Mozilla/5.0 (Mobile; rv:18.0) Gecko/18.0 Firefox/18.0")
+    RulesStringValue(19,"&Firefox (Mac)", "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.8; rv:24.0) Gecko/20100101 Firefox/24.0")
+    RulesStringValue(20,"Chrome (Win)", "Mozilla/5.0 (Windows NT 6.3; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/48.0.2564.48 Safari/537.36")
+    RulesStringValue(21,"Chrome (Android)", "Mozilla/5.0 (Linux; Android 5.1.1; Nexus 5 Build/LMY48B) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/43.0.2357.78 Mobile Safari/537.36")
+    RulesStringValue(22,"ChromeBook", "Mozilla/5.0 (X11; CrOS x86_64 6680.52.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2272.74 Safari/537.36")
+    RulesStringValue(23,"GoogleBot Crawler", "Mozilla/5.0 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)")
+    RulesStringValue(24,"Kindle Fire (Silk)", "Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10_6_3; en-us; Silk/1.0.22.79_10013310) AppleWebKit/533.16 (KHTML, like Gecko) Version/5.0 Safari/533.16 Silk-Accelerated=true")
+    RulesStringValue(25,"&Custom...", "%CUSTOM%")
+    public static var sUA: String = null;
+
+    public static RulesOption("Simulate &Modem Speeds", "Per&formance")
+    var m_SimulateModem: boolean = false;
+
+    public static RulesOption("&Disable Caching", "Per&formance")
+    var m_DisableCaching: boolean = false;
+
+    public static RulesOption("Cache Always &Fresh", "Per&formance")
+    var m_AlwaysFresh: boolean = false;
+        
+    public static ToolsAction("Reset Script")
+    function DoManualReload() { 
+        FiddlerObject.ReloadScript();
+    }
+
+    public static ContextAction("Decode Selected Sessions")
+    function DoRemoveEncoding(oSessions: Session[]) {}
+    static function OnBeforeRequest(oSession: Session) {}
+    static function OnPeekAtResponseHeaders(oSession: Session) {}
+
+    static function OnBeforeResponse(oSession: Session) {
+        
+        if(oSession.fullUrl.Contains("nexusmods.com") && oSession.fullUrl.Contains("file_id"))
+        {
+            oSession.utilDecodeResponse();
+            var oBody = System.Text.Encoding.UTF8.GetString(oSession.responseBodyBytes);
+            oBody = oBody.replace("<button id=\"slowDownloadButton\"", "<script>setTimeout(() => { document.getElementById(\"slowDownloadButton\").click() }, 100);</script><button id=\"slowDownloadButton\"");
+            oBody = oBody.replace("countdown(timeout, function () {\n                startDownload(downloadUrl);\n            });\n", "startDownload(downloadUrl);");
+            oSession.utilSetResponseBody(oBody); 
+        }
+        
+    }
+		
+    static function Main() {}
+
+    BindPref("fiddlerscript.ephemeral.bpRequestURI")
+    public static var bpRequestURI:String = null;
+
+    BindPref("fiddlerscript.ephemeral.bpResponseURI")
+    public static var bpResponseURI:String = null;
+
+    BindPref("fiddlerscript.ephemeral.bpMethod")
+    public static var bpMethod: String = null;
+
+    static var bpStatus:int = -1;
+    static var uiBoldURI: String = null;
+    static var gs_ReplaceToken: String = null;
+    static var gs_ReplaceTokenWith: String = null;
+    static var gs_OverridenHost: String = null;
+    static var gs_OverrideHostWith: String = null;
+		
+    static function OnExecAction(sParams: String[]): Boolean {}
+}
